51f8e5be081fdcd77787adeebfa3200698d58074
Ariful Haque Tomal <87800317+arifulhaquetomal@users.noreply.github.com>
2026-01-28 14:01:19 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // configuration for the chatbot
 
-const GEMINI_API_KEY;
+let GEMINI_API_KEY;
 // export const GEMINI_API_KEY = TODO_API_KEY;
 
 // For simple inclusion in index.html without modules:
